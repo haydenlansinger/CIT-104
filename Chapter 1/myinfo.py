203625dd2a156f19134f164d0e251a0100b5c7db
Hayden <65086111+haydenlansinger@users.noreply.github.com>
2026-08-31 08:19:23 -0400

    Initial commit

---
+++ Chapter 1/myinfo.py
@@ -0,0 +1,14 @@
+# ----------------------------------
+# Program: myinfo.py
+# Author: Hayden Lansinger
+# Date: 8/28/2026
+# Description: This program displays my personal information including my name, address, and phone number.
+# ----------------------------------
+
+name = "Hayden Lansinger"
+address = "742 Evergreen Terrace, Springfield, USA"
+phone_number = "(800) 555-0000"
+
+print("Name:", name)
+print("Address:", address)
+print("Phone Number:", phone_number)
