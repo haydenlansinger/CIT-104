d9078aa58d529e4ada2f0de5604163e513f3d6ca
Hayden <65086111+haydenlansinger@users.noreply.github.com>
2026-08-31 09:01:57 -0400

    submit chapter 2 work

---
+++ Chapter 3/one.py
@@ -0,0 +1,12 @@
+# ------------------------------
+# Name: one.py
+# Author: Hayden Lansinger
+# Date: 8/31/2026
+# Description: Python program to add three numbers.
+# ------------------------------
+
+num1 = 20
+num2 = 10
+num3 = 30
+total = num1 + num2 + num3
+print(total)
---
+++ Chapter 3/two.py
@@ -0,0 +1,10 @@
+# ------------------------------
+# Name: two.py
+# Author: Hayden Lansinger
+# Date: 8/31/2026
+# Description: Python program to find the max of numbers.
+# ------------------------------
+
+numbers = [40,50,20,10,30]
+max_number = max(numbers)
+print(max_number)
