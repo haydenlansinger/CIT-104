85a06337913819fd63fa61af36ce22c4fd99457a
Hayden <65086111+haydenlansinger@users.noreply.github.com>
2026-09-12 20:16:36 -0400

    fix assignment

--- Chapter 3/main.py
+++ Chapter 3/main.py
@@ -2,22 +2,28 @@
 # Name: main.py
 # Author: Hayden Lansinger
 # Date: 9/2/2026
-# Description: This program finds all prime numbers up to a number entered by the user.
+# Description: This program will perform a prime factorization of a number between 2 and 100 and decide if the number is prime or not.
 # ------------------------------
 
 num = int(input("Enter a number between 2 and 100: "))
 while num > 100 or num < 2:
     num = int(input("Enter a number between 2 and 100: "))
 
-primes = []
-# for every number between 2 and the input number, check if it is prime
+factors = []
+# loop between 2 and num
 for i in range(2, num + 1):
-    is_prime = True # assume the number is prime until proven otherwise
-    for j in range(2, int(i ** 0.5) + 1): # check divisibility up to the square root of the current number
-        if i % j == 0: # if remainder is 0, number isnt prime
-            is_prime = False
-            break
-    if is_prime: # if prime, add prime number to list
-        primes.append(i)
+    # if remainder is 0 then it is a factor
+    while num % i == 0:
+        # add it to the list
+        factors.append(i)
+        num //= i
 
-print("Prime numbers are", str(primes))
+# check for extra factor
+if num > 1:
+    factors.append(num)
+
+# print results
+if len(factors) == 1:
+    print("PRIME")
+else:
+    print(str(factors))
